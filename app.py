30e73173cbd6edd507844ef80cbc93f948ef437f
karun <mailto.karun.py@gmail.com>
2022-11-04 11:21:57 +0530

    INITIAL COMMIT - 5

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route("/")
 def hello():
-    return "Hello, Chetak!"
-
+    return "Hello, Hi!"
 
 if __name__ == '__main__':
     app.run()
